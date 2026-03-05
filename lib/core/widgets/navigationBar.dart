c14fd83fcbd53de5702acf83851d02f6806204c4
angelabuxo <angiangeleta2@gmail.com>
2026-03-05 12:42:27 +0100

    feat: add bottom navigation bar widget

---
+++ lib/core/widgets/navigationBar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AgendatBottomNavigationBar extends StatelessWidget {
+  const AgendatBottomNavigationBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: const Color.fromARGB(255, 152, 38, 30),
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.white70,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Inici'),
+        BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Mapa'),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_month),
+          label: 'Calendari',
+        ),
+        BottomNavigationBarItem(icon: Icon(Icons.chat_bubble), label: 'Social'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Perfil'),
+      ],
+    );
+  }
+}
